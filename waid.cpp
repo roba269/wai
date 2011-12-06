f3686e18ef9c83994e44ba12b5a6f1c148130a90
He Liang <roba269@gmail.com>
2011-12-06 16:10:24 +0000

    add waid.cpp

---
+++ waid.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char **argv)
+{
+    // 1. Create Deamon
+    // 2. poll query in database, compiling uncompiled source file
+    // 3. call Scheduler::GetMatch(), and build match, get result, fill in the db
+    return 0;
+}
+
