84a034b0dd7b8674c0a69d2a4f678273dd4559bf
Liang He <roba269@gmail.com>
2012-08-18 16:09:24 +0800

    add waiconst

---
+++ web/waiconst.js
@@ -0,0 +1,5 @@
+exports.USER_SRC_PATH = '/var/wai/uploads/';
+exports.USER_EXE_PATH = '/var/wai/exe/';
+exports.JUDGE_PATH = '/var/wai/judge/';
+exports.MATCH_PATH = '/var/wai/match/';
+exports.LOG_PATH = '/var/log/wai/';
